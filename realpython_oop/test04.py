0a858fef51ccd1f77013fae4ed75c5936c12fff3
Student Space <nioufarrahimizade@yahoo.com>
2021-07-06 18:59:07 +0430

    15Tir]

--- realpython_oop/test04.py
+++ realpython_oop/test04.py
@@ -1,1 +1,4 @@
 # Working with Classes and Instances
+# You can use classes to represent many real-world situations. Once you write
+# a class, you’ll spend most of your time working with instances created from
+# that class.
